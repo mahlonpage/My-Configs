c9c22f8e2d03e278deaef1f9cfe14cff1702bb97
Mahlon Page <mahlon_page@brown.edu>
2023-03-01 22:27:02 -0500

    restructed .zshrc into separate files

---
+++ zsh/classes.zsh
@@ -0,0 +1,31 @@
+#Data Science Virtual Environment
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#Data Science Python 3.7.9 work around
+alias ibrew="arch -x86_64 /usr/local/bin/brew"
+
+#Each function represents the actions to route into a given classes folder and run relative commands
+#They end in nc to represent "no code" pairing with the normal version aliases that append 'code .'
+
+export COURSE_LOC=~/Documents/cs/curr_classes/ #Course folder location
+
+function datanc() {
+    source ~/cs1951a_venv/bin/activate
+    cd ${COURSE_LOC}data/
+}
+
+function dlnc()   {
+    conda activate csci1470
+    cd ${COURSE_LOC}dl/
+}
+
+function cybernc()   {
+    cd ${COURSE_LOC}cyber/dev-env/
+}
+
+#Current Courses (but also opens vscode)
+alias data='datanc; code .;'
+alias dl='dlnc; code .;'
+alias cyber='cybernc; code .;'
---
+++ zsh/personal.zsh
@@ -0,0 +1,27 @@
+#My Aliases
+alias c='clear'
+alias cs='cd ~/Documents/git_lc/'
+alias z='cd ~/dot-files/zsh; ls'
+alias v='code .'
+alias s='sqlite3'
+alias p='python3'
+#Takes song Zack is currently listening to and adds it to my playlist
+alias a='/Users/mahlonpage/Documents/cs/tuningfork/venv/bin/python3 /Users/mahlonpage/Documents/cs/tuningfork/enqueue.py -zis'
+#Shows song Zack is listening to.
+alias zack='/Users/mahlonpage/Documents/cs/tuningfork/venv/bin/python3 /Users/mahlonpage/Documents/cs/tuningfork/enqueue.py -zi'
+
+#My Functions
+function def()  {
+  cd ~/
+  $aliases[c]
+  conda deactivate &> /dev/null
+  deactivate &> /dev/null
+  exec $SHELL -l
+}
+
+function gall() {
+  git add -A
+  git commit -m "$([ "$1" != "" ] && echo "$1" || echo "minor tweak")"
+  git push
+}
+
