3f5844b2489b337f9448452143f19b7105829ca0
Mahlon Page <mahlon_page@brown.edu>
2023-03-16 12:13:06 -0400

    added gitconfig

--- zsh/classes.zsh
+++ zsh/classes.zsh
@@ -3,6 +3,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Sets Java 1.8 for PySpark for Data Science Project MapReduce
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home
+
 #Data Science Python 3.7.9 work around
 alias ibrew="arch -x86_64 /usr/local/bin/brew"
 
