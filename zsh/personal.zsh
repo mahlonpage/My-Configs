85ecf1f4eb6e7c23dec7a8d327856410b92b3243
Mahlon Page <mahlon_page@brown.edu>
2023-06-23 18:09:02 -0400

    updated regex for rENGex i wrote

--- zsh/personal.zsh
+++ zsh/personal.zsh
@@ -6,11 +6,16 @@ alias p='python3'
 alias ls='lsd' #use lsd for pretty printing
 alias please='sudo $(fc -ln -1)'
 
+function regex()    {
+    /usr/local/bin/python3 /Users/mahlonpage/Documents/cs/rENGex/code/rENGex.py $@
+}
+
 #Enqueue
+MUSIC='/Users/mahlonpage/Documents/cs/z\ Tuningfork/venv/bin/python3 /Users/mahlonpage/Documents/cs/z\ Tuningfork/enqueue.py'
 #Takes song Zack is currently listening to and adds it to my playlist
-alias a='/Users/mahlonpage/Documents/cs/tuningfork/venv/bin/python3 /Users/mahlonpage/Documents/cs/tuningfork/enqueue.py -zis'
+alias a="$MUSIC -zis"
 #Shows song Zack is listening to.
-alias zack='/Users/mahlonpage/Documents/cs/tuningfork/venv/bin/python3 /Users/mahlonpage/Documents/cs/tuningfork/enqueue.py -zi'
+alias zack="$MUSIC -zi"
 
 # My functions
 
