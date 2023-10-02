ea8f3c27e591e023bfb10bce54a51193efb87185
Mahlon Page <mahlon_page@brown.edu>
2023-10-01 21:29:48 -0400

    cleared optiver assessment venv

--- zsh/personal.zsh
+++ zsh/personal.zsh
@@ -4,7 +4,6 @@ alias cs='cd ~/Documents/cs/'
 alias s='sqlite3'
 alias p='python3'
 alias ls='lsd' #use lsd for pretty printing
-alias opt='source ./Documents/cs/HFT\ Researcher\ Assessment/optiver/bin/activate'
 
 function regex()    {
     /usr/local/bin/python3 /Users/mahlonpage/Documents/cs/rENGex/code/rENGex.py $@
