a24c2fa9cee63a8016e5dc435921f665f878301e
Mahlon Page <mahlon_page@brown.edu>
2023-12-28 14:55:53 -0500

    removed data science venv info

--- zsh/classes.zsh
+++ zsh/classes.zsh
@@ -1,8 +1,3 @@
-#Data Science Virtual Environment
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 #Each function represents the actions to route into a given classes folder and run relative commands
 #They end in nc to represent "no code" pairing with the normal version aliases that append 'code .'
 
