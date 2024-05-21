a86ea20a508474f33418dcfb21757e9e17b27704
Mahlon Page <mahlon_page@brown.edu>
2024-05-21 13:50:08 -0400

    removed research and classes. end of semester

--- zsh/classes.zsh
+++ zsh/classes.zsh
@@ -6,36 +6,19 @@ export COURSE_LOC=~/Documents/cs/classes/ #Course folder location
 function cdclass() {
     case $1 in
 
-        "temp")
-            cd ${COURSE_LOC}temp
-            ;;
-        "logic")
-            cd ${COURSE_LOC}1710
-            ;;
-        "agt")
-            cd ${COURSE_LOC}2440
-            ;;
+#        "logic")
+#            cd ${COURSE_LOC}1710
+#            ;;
         *)
             echo "Error: Usage cdclass with arg1 = PLACEHOLDER TEXT"
             ;;
     esac
 }
 
-function logic() {
-    cdclass "logic"
-    venv
-}
-
-function agt() {
-    cdclass "agt"
-    venv
-}
-
-function res()    {
-    cd ~/Documents/cs/research/code
-}
+#function logic() {
+#    cdclass "logic"
+#    venv
+#}
 
 #Current Courses (but also opens vscode)
-alias logicc='logic; code .;'
-alias agtc='agt; code .;'
-alias resc='res; venv; code .;'
+#alias logicc='logic; code .;'
